55e3c09621aa1859652d0186866cf7c3fab8e443
FrederikHTA <frederikhta@gmail.com>
2025-08-04 17:40:18 +0200

    formatting

--- src/main.zig
+++ src/main.zig
@@ -111,9 +111,9 @@ fn handleMovement(state: *game.GameState) void {
             const block_definition = b.getBlockDefinition(saved_block);
             var saved_active_block = state.active_block;
             saved_active_block.block_definition = block_definition;
-            
+
             const can_rotate = game.canRotateBlockWithWallKick(saved_active_block, state.grid, block_definition.rotation);
-            if(can_rotate.success) {
+            if  (can_rotate.success) {
                 state.active_block.block_definition = block_definition;
                 state.active_block.x = state.active_block.x + can_rotate.x_offset;
                 state.saved_block = null;
